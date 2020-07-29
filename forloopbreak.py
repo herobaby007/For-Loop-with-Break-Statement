cc1413f72aedeeb4dee53d6c251bed0197953ddc
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-07-29 16:05:20 +0530

    Add files via upload

---
+++ forloopbreak.py
@@ -0,0 +1,16 @@
+l = ["Python", "Java", "C++", "Javascript"]
+
+
+keyword = "Python"
+
+for value in l:
+
+	if value == keyword:
+	   print("Keyword Found")
+	   break
+	else:
+	   continue
+
+else:
+	   print("Keyword not found")   
+
